731646b3a60efcdbab42f73bb8d8627ec4f365e2
Anusha-Qasim <168641167+Anusha-Qasim@users.noreply.github.com>
2024-05-09 23:39:00 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const systemnumber = Math.floor(Math.random() * 6);
+const answer = await inquirer.prompt([
+    {
+        message: "choose randomnumber between 1-6",
+        type: "number",
+        name: "randomnumber",
+    }
+]);
+if (answer.randomnumber === systemnumber) {
+    console.log("congratulations! you choose a correct number");
+}
+else {
+    console.log("you choose a wrong number");
+}
